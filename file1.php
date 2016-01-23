57ba5a858f96b7c9544f3d6cb3abfc1ab31be31c
Long Do <candy_man_tl@yahoo.com>
2016-01-23 14:01:52 +0700

    first commit

---
+++ file1.php
@@ -0,0 +1,14 @@
+<?php
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testA()
+    {
+        $this->assertEquals(1,1);
+    }
+    
+    public function testA1()
+    {
+        $this->assertEquals(1,1);
+    }
+}
+?>
